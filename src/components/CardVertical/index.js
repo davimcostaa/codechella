eb32b193906be16ee6684d3acf67a76b755a34f3
Davi Costa <davim.costa@outlook.com>
2023-05-16 21:45:34 -0300

    Cards - página de mapa

--- src/components/CardVertical/index.js
+++ src/components/CardVertical/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from './CardVertical.module.css'
 
-const CardVertical = () => {
+const CardVertical = ( {imagem, titulo, texto} ) => {
   return (
-    <div>CardVertical</div>
+    <div className={styles.container}>
+          <div>
+                <img 
+                alt='Foto'
+                src={`/imagens/paginas/${imagem}.png`}
+                className={styles.imagem}
+                />
+            </div>
+
+          <div className={styles.textos}>
+              <h2 className={styles.titulo}>{titulo}</h2>
+              <p className={styles.texto}>
+                  {texto}
+              </p>
+          </div>
+
+         </div> 
   )
 }
 
--- src/pages/Mapa/Mapa.js
+++ src/pages/Mapa/Mapa.js
@@ -1,4 +1,5 @@
 import Banner from 'components/Banner'
+import CardVertical from 'components/CardVertical'
 import Legenda from 'components/Legenda'
 import Menu from 'components/Menu'
 import Rodape from 'components/Rodape'
@@ -26,8 +27,19 @@ const Mapa = () => {
               <Legenda imagem="cadeiras-terreo" legenda="Cadeiras térreo"/>
               <Legenda imagem="cadeiras-superior" legenda="Cadeiras superiores"/>
         </div>
+
     </section>
-    
+
+    <section>
+
+    <h2 className={styles.tituloSetores}>Mais detalhes sobre os setores:</h2>
+
+    <div className={styles.cards}>
+      <CardVertical imagem="Mapa de setores2" titulo="Pista" texto="Pista convencional, atendida pelos bares e banheiros das laterais do estádio. Espaço amplo, com local para sentar e descansar. Separada da pista premium por uma grade."/>
+      <CardVertical imagem="Mapa de setores3" titulo="Pista Premium" texto="Pista mais próxima do palco, com acesso muito próximo a banheiros e bares num raio de 100m. Todas as pessoas idosas e/ou com deficiência têm direito a acesso gratuito a essa área."/>
+      <CardVertical imagem="Mapa de setores4" titulo="Cadeiras" texto="Opção de cadeiras térreas ou elevadas. Acesso próximo a banheiros e bares, visão um pouco elevada em relação às pistas."/>
+    </div>
+    </section>
     </section>
     <Rodape />
     </>
