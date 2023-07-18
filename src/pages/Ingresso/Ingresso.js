72722e0c4a51aee4948af98b12ad68e6f8b21b33
Davi Costa <davim.costa@outlook.com>
2023-07-17 23:56:10 -0300

    Ingresso com dados mocados

--- src/pages/Ingresso/Ingresso.js
+++ src/pages/Ingresso/Ingresso.js
@@ -1,13 +1,13 @@
 import { IngressoContext } from 'common/context/Ingresso';
 import Banner from 'components/Banner'
 import Menu from 'components/Menu'
+import Rodape from 'components/Rodape';
 import React, { useContext } from 'react'
+import styles from './Ingresso.module.css'
 
 const Ingresso = () => {
 
   const {  formData  } = useContext(IngressoContext);
-
-  console.log(formData)
   
   return (
     <>
@@ -15,6 +15,36 @@ const Ingresso = () => {
         <Menu />
         <Banner imagem="ingresso2" titulo="Seu ingresso está aqui!" />
 
+        <section className={styles.conteudo}>
+            <h2 className={styles.titulo}>Uhul, agora sim! Seu ingresso está aqui, <br /> apresente na entrada do evento e divirta-se!</h2>
+
+            
+            <div className={styles.ingresso}>
+              <div className={styles.topo}>
+                  <img src='/imagens/logo-cinza.png' />
+                  <img src='/imagens/favicon-branco.png' />
+      
+              </div>
+
+              <div className={styles.info}>
+                  <div>
+                    <img src='/imagens/Qr code 1.png' />
+                  </div>
+
+                  <div>
+                      <h3 className={styles.info_titulo}>Moni Hillman</h3>
+
+                      <p className={styles.infos}>Ingresso Cortesia</p>
+                      <p className={styles.infos}>Setor Pista</p>
+                      <p className={styles.infos}>Data: 11/03</p>
+                      <p className={styles.infos}>Local: São Paulo</p>
+
+                  </div>
+              </div>
+            </div>
+        </section>
+
+    <Rodape />
     
     </>
   )
